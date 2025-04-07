87cff8b30df8ffaf01c04156cfd95ee66abd2e90
Luca Lamoni <lucalamoni@gmail.com>
2025-04-07 10:58:58 +0200

    tried to update vms data but it does not seem to recognise the file

--- data-raw/data.R
+++ data-raw/data.R
@@ -3,40 +3,53 @@ library(sf)
 library(leaflet)
 library(icesVMS)
 
-mkdir("data/VMS")
+# mkdir("data/VMS")
 
-ecoregions <- c("Baltic Sea", "Bay of Biscay and the Iberian Coast", "Celtic Seas", "Greater North Sea",  "Norwegian Sea", "Icelandic Waters", "Barents Sea", "Greenland Sea", "Faroes", "Oceanic Northeast Atlantic", "Azores")
+# ecoregions <- c("Baltic Sea", "Bay of Biscay and the Iberian Coast", "Celtic Seas", "Greater North Sea",  "Norwegian Sea", "Icelandic Waters", "Barents Sea", "Greenland Sea", "Faroes", "Oceanic Northeast Atlantic", "Azores")
 
 
-# Get the VMS effort for all ecoregions and save each file as a rda file in data/VMS
+# # Get the VMS effort for all ecoregions and save each file as a rda file in data/VMS
+# for (ecoregion in ecoregions) {
+#     vms_effort <- icesVMS::get_effort_map(ecoregion, year = 2022)
+#     vms_sar <- icesVMS::get_sar_map(ecoregion, year = 2022)
+#     # convert to sf
+#     vms_effort$wkt <- sf::st_as_sfc(vms_effort$wkt)
+#     vms_effort <- sf::st_sf(vms_effort, sf_column_name = "wkt", crs = 4326)
+#     # convert to sf
+#     vms_sar$wkt <- sf::st_as_sfc(vms_sar$wkt)
+#     vms_sar <- sf::st_sf(vms_sar, sf_column_name = "wkt", crs = 4326)
+
+#     saveRDS(vms_effort, paste0("data/VMS/vms_effort_", ecoregion, ".rda"))
+#     saveRDS(vms_sar, paste0("data/VMS/vms_sar_", ecoregion, ".rda"))
+# }
+
+ecoregions <- c(
+  "Baltic Sea", "Bay of Biscay and the Iberian Coast", "Celtic Seas",
+  "Greater North Sea", "Norwegian Sea", "Icelandic Waters", "Barents Sea",
+  "Greenland Sea", "Faroes", "Oceanic Northeast Atlantic", "Azores"
+)
+
+# Create empty lists to store the sf objects
+effort_list <- list()
+sar_list <- list()
+
+# Loop through ecoregions and populate the lists
 for (ecoregion in ecoregions) {
-    vms_effort <- icesVMS::get_effort_map(ecoregion, year = 2022)
-    vms_sar <- icesVMS::get_sar_map(ecoregion, year = 2022)
-    # convert to sf
-    vms_effort$wkt <- sf::st_as_sfc(vms_effort$wkt)
-    vms_effort <- sf::st_sf(vms_effort, sf_column_name = "wkt", crs = 4326)
-    # convert to sf
-    vms_sar$wkt <- sf::st_as_sfc(vms_sar$wkt)
-    vms_sar <- sf::st_sf(vms_sar, sf_column_name = "wkt", crs = 4326)
+  vms_effort <- icesVMS::get_effort_map(ecoregion, year = 2022)
+  vms_sar <- icesVMS::get_sar_map(ecoregion, year = 2022)
 
-    saveRDS(vms_effort, paste0("data/VMS/vms_effort_", ecoregion, ".rda"))
-    saveRDS(vms_sar, paste0("data/VMS/vms_sar_", ecoregion, ".rda"))
+  # Convert to sf
+  vms_effort$wkt <- sf::st_as_sfc(vms_effort$wkt)
+  vms_effort <- sf::st_sf(vms_effort, sf_column_name = "wkt", crs = 4326)
+
+  vms_sar$wkt <- sf::st_as_sfc(vms_sar$wkt)
+  vms_sar <- sf::st_sf(vms_sar, sf_column_name = "wkt", crs = 4326)
+
+  # Add to lists with ecoregion name as the key
+  effort_list[[ecoregion]] <- vms_effort
+  sar_list[[ecoregion]] <- vms_sar
 }
 
+save(effort_list, file = "./data/vms_effort_all.rda")
+save(sar_list, file = "./data/vms_sar_all.rda")
 
-# # icesVMS::update_token("colin")
-# vms_sar <- icesVMS::get_sar_map(ecoregion, year = 2022)
-
-# # convert to sf
-# vms_sar$wkt <- sf::st_as_sfc(vms_sar$wkt)
-# vms_sar <- sf::st_sf(vms_sar, sf_column_name = "wkt", crs = 4326)
-
-# sf::st_write(vms_sar, paste0("vms_sar_", ecoregion, ".csv"), layer_options = "GEOMETRY=AS_WKT")
-
-# vms_effort <- icesVMS::get_effort_map(ecoregion, year = 2022)
-
-# # convert to sf
-# vms_effort$wkt <- sf::st_as_sfc(vms_effort$wkt)
-# vms_effort <- sf::st_sf(vms_effort, sf_column_name = "wkt", crs = 4326)
-
-# sf::st_write(vms_effort, paste0("vms_effort_", ecoregion, ".csv"), layer_options = "GEOMETRY=AS_WKT")
